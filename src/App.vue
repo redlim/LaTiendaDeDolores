c8a0502ff9cae3eaf2acecc94f955e505b220ffd
Will <wolmos@m4v.es>
2018-02-24 17:46:39 +0100

    First scafolding and adding a simple pages structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="overlay">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html, body {
+    margin: 0;
+    height: 100%;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+</style>
---
+++ src/pages/Welcome.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hero-image">
+    <div class="overlay">
+      <div class="hero-text">
+        <h1>Bienvenidos a <strong>El mercado de Dolores</strong></h1>
+        <label for="inputPostalCode">Código Postal:</label>
+        <input id="inputPostalCode" type="text" v-model="postalCode">
+        <button v-clicK="goToShops">Buscar Tiendas</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome',
+    data () {
+      return {
+        postalCode:''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  .hero-image {
+    background-image: url('../assets/background.jpeg');
+    height: 70%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  }
+  .hero-text {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+  }
+  .overlay{
+    height: 100%;
+    width: 100%;
+    background: rgba(0,0,0,0.3); ;
+  }
+</style>
